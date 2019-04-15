62f23eabcb8f3644a5cef2d530db4d0b54ee2e94
Abdoul Karim <abdoulkarimsavadogo03@gmail.com>
2019-04-15 10:22:47 +0000

    Initial Project

---
+++ server/src/main/java/com/burkinabe/spendingmanager/SpendingmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.burkinabe.spendingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpendingmanagerApplication.class, args);
+    }
+
+}
